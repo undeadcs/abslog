75d71c93b418c98ed1cf4f3c9d15aa90ae7dbdcf
Malyshev Alexander <_undead_@mail.ru>
2023-10-04 23:16:36 +1000

    tests - testing with time field

--- php/tests/ExtendingTests.php
+++ php/tests/ExtendingTests.php
@@ -44,8 +44,8 @@ class ServiceRecord extends Record {
 	 */
 	public int $category = self::CATEGORY_MESSAGE;
 	
-	public function __construct( string $message, int $level = self::LVL_INFO, string $host = '', int $pid = 0, string $name = '', int $category = self::CATEGORY_MESSAGE ) {
-		parent::__construct( $message, $level );
+	public function __construct( string $message, int $level = self::LVL_INFO, \DateTime $time = new \DateTime, string $host = '', int $pid = 0, string $name = '', int $category = self::CATEGORY_MESSAGE ) {
+		parent::__construct( $message, $level, $time );
 		
 		$this->host		= $host;
 		$this->pid		= $pid;
@@ -57,7 +57,7 @@ class ServiceRecord extends Record {
 	 * Magic method for casting to string
 	 */
 	public function __toString( ) : string {
-		return '['.self::LevelToString( $this->level ).']['.$this->host.']['.$this->pid.']['.$this->name.']['.$this->category.'] '.$this->message;
+		return '['.$this->time->format( 'c' ).']['.self::LevelToString( $this->level ).']['.$this->host.']['.$this->pid.']['.$this->name.']['.$this->category.'] '.$this->message;
 	}
 }
 
@@ -66,18 +66,21 @@ class ServiceRecord extends Record {
  */
 class ExtendingTests extends TestCase {
 	public static function basicValuesProvider( ) : array {
+		$time = new \DateTime;
+		$timeStr = $time->format( 'c' );
+		
 		return [
-			[ 'error message',		ServiceRecord::LVL_ERROR,		'Error',	'127.0.0.1',	106458,	'service1',	ServiceRecord::CATEGORY_COMMAND,
-				'[ERROR][127.0.0.1][106458][service1]['.ServiceRecord::CATEGORY_COMMAND.'] error message'
+			[ 'error message',		ServiceRecord::LVL_ERROR,		'Error',	$time,	'127.0.0.1',	106458,	'service1',	ServiceRecord::CATEGORY_COMMAND,
+				'['.$timeStr.'][ERROR][127.0.0.1][106458][service1]['.ServiceRecord::CATEGORY_COMMAND.'] error message'
 			],
-			[ 'warning message',	ServiceRecord::LVL_WARNING,	'Warning',	'127.0.0.2',	106459,	'service2',	ServiceRecord::CATEGORY_EVENT,
-				'[WARNING][127.0.0.2][106459][service2]['.ServiceRecord::CATEGORY_EVENT.'] warning message'
+			[ 'warning message',	ServiceRecord::LVL_WARNING,	'Warning',		$time,	'127.0.0.2',	106459,	'service2',	ServiceRecord::CATEGORY_EVENT,
+				'['.$timeStr.'][WARNING][127.0.0.2][106459][service2]['.ServiceRecord::CATEGORY_EVENT.'] warning message'
 			],
-			[ 'info message',		ServiceRecord::LVL_INFO,		'Info',		'127.0.0.3',	106460,	'service3',	ServiceRecord::CATEGORY_COMMAND,
-				'[INFO][127.0.0.3][106460][service3]['.ServiceRecord::CATEGORY_COMMAND.'] info message'
+			[ 'info message',		ServiceRecord::LVL_INFO,		'Info',		$time,	'127.0.0.3',	106460,	'service3',	ServiceRecord::CATEGORY_COMMAND,
+				'['.$timeStr.'][INFO][127.0.0.3][106460][service3]['.ServiceRecord::CATEGORY_COMMAND.'] info message'
 			],
-			[ 'debug message',		ServiceRecord::LVL_DEBUG,		'Debug',	'127.0.0.4',	106461,	'service4',	ServiceRecord::CATEGORY_MESSAGE,
-				'[DEBUG][127.0.0.4][106461][service4]['.ServiceRecord::CATEGORY_MESSAGE.'] debug message'
+			[ 'debug message',		ServiceRecord::LVL_DEBUG,		'Debug',	$time,	'127.0.0.4',	106461,	'service4',	ServiceRecord::CATEGORY_MESSAGE,
+				'['.$timeStr.'][DEBUG][127.0.0.4][106461][service4]['.ServiceRecord::CATEGORY_MESSAGE.'] debug message'
 			]
 		];
 	}
@@ -86,8 +89,9 @@ class ExtendingTests extends TestCase {
 	 * Testing basic fields
 	 */
 	#[ DataProvider( 'basicValuesProvider' ) ]
-	public function testValues( string $message, int $level, string $fn, string $host, int $pid, string $name, int $category, string $expected ) : void {
+	public function testValues( string $message, int $level, string $fn, \DateTime $time, string $host, int $pid, string $name, int $category, string $expected ) : void {
 		$actual = ServiceRecord::$fn( $message, $host, $pid, $name, $category );
+		$actual->time = $time; // just for equals
 		
 		$this->assertEquals( $message, $actual->message );
 		$this->assertEquals( $level, $actual->level );
@@ -95,7 +99,7 @@ class ExtendingTests extends TestCase {
 		$this->assertEquals( $pid, $actual->pid );
 		$this->assertEquals( $name, $actual->name );
 		$this->assertEquals( $category, $actual->category );
-		$this->assertEquals( new ServiceRecord( $message, $level, $host, $pid, $name, $category ), $actual );
+		$this->assertEquals( new ServiceRecord( $message, $level, $time, $host, $pid, $name, $category ), $actual );
 		$this->assertEquals( $expected, ( string ) $actual );
 	}
 }
--- php/tests/FileTests.php
+++ php/tests/FileTests.php
@@ -34,11 +34,14 @@ class FileTests extends TestCase {
 	}
 	
 	public static function fileMessagesProvider( ) : array {
+		$time = new \DateTime;
+		$timeStr = $time->format( 'c' );
+		
 		return [
-			[ 'error message',		Record::LVL_ERROR,		'Error',	"[ERROR] error message\n"		],
-			[ 'warning message',	Record::LVL_WARNING,	'Warning',	"[WARNING] warning message\n"	],
-			[ 'info message',		Record::LVL_INFO,		'Info',		"[INFO] info message\n"			],
-			[ 'debug message',		Record::LVL_DEBUG,		'Debug',	"[DEBUG] debug message\n"		]
+			[ 'error message',		Record::LVL_ERROR,		'Error',	$time, "[$timeStr][ERROR] error message\n"		],
+			[ 'warning message',	Record::LVL_WARNING,	'Warning',	$time, "[$timeStr][WARNING] warning message\n"	],
+			[ 'info message',		Record::LVL_INFO,		'Info',		$time, "[$timeStr][INFO] info message\n"		],
+			[ 'debug message',		Record::LVL_DEBUG,		'Debug',	$time, "[$timeStr][DEBUG] debug message\n"		]
 		];
 	}
 	
@@ -46,9 +49,11 @@ class FileTests extends TestCase {
 	 * Test file by name
 	 */
 	#[ DataProvider( 'fileMessagesProvider' ) ]
-	public function testFile( string $message, int $level, string $fn, string $expected ) : void {
+	public function testFile( string $message, int $level, string $fn, \DateTime $time, string $expected ) : void {
 		$log = new FileLog( $this->tmpFilename );
-		$log->Write( Record::$fn( $message ) );
+		$record = Record::$fn( $message );
+		$record->time = $time; // just for equals
+		$log->Write( $record );
 		
 		$actual = file_get_contents( $this->tmpFilename );
 		$this->assertEquals( $expected, $actual );
@@ -58,9 +63,11 @@ class FileTests extends TestCase {
 	 * Test file by descriptor
 	 */
 	#[ DataProvider( 'fileMessagesProvider' ) ]
-	public function testDescriptor( string $message, int $level, string $fn, string $expected ) : void {
+	public function testDescriptor( string $message, int $level, string $fn, \DateTime $time, string $expected ) : void {
 		$log = new DescriptorLog( $this->tmpFd );
-		$log->Write( Record::$fn( $message ) );
+		$record = Record::$fn( $message );
+		$record->time = $time; // just for equals
+		$log->Write( $record );
 		
 		$actual = file_get_contents( $this->tmpFilename );
 		$this->assertEquals( $expected, $actual );
@@ -72,16 +79,20 @@ class FileTests extends TestCase {
 	protected function testMultilog( ) : void {
 		$filename1 = tempnam( __DIR__, 'abslogtest1_' );
 		$filename2 = tempnam( __DIR__, 'abslogtest2_' );
+		$time = new \DateTime;
+		$timeStr = $time->format( 'c' );
 		
 		$log = new MultiLog( new FileLog( $filename1 ), new FileLog( $filename2 ) );
-		$log->Write( Record::Info( 'info message' ) );
+		$record = Record::Info( 'info message' );
+		$record->time = $time; // just for equals
+		$log->Write( $record );
 		
 		$actual1 = file_get_contents( $filename1 );
 		$actual2 = file_get_contents( $filename2 );
 		unlink( $filename1 );
 		unlink( $filename2 );
 		
-		$expected = "[INFO] info message\n";
+		$expected = "[$timeStr][INFO] info message\n";
 		$this->assertEquals( $expected, $actual1 );
 		$this->assertEquals( $expected, $actual2 );
 	}
